dd2421f4254aedfb1cf10a7f3c0e2747bba08b8b
martyneczeq <martynajablonka99@gmail.com>
2024-06-16 12:35:56 +0200

    add client entity

---
+++ src/Entity/Client.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ClientRepository::class)]
+class Client
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $secondName = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $pesel = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSecondName(): ?string
+    {
+        return $this->secondName;
+    }
+
+    public function setSecondName(?string $secondName): static
+    {
+        $this->secondName = $secondName;
+
+        return $this;
+    }
+
+    public function getPesel(): ?int
+    {
+        return $this->pesel;
+    }
+
+    public function setPesel(?int $pesel): static
+    {
+        $this->pesel = $pesel;
+
+        return $this;
+    }
+}
